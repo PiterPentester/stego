174a75fed1a4e1102ddc6a1813f74bbed3f97c39
Bohdan Pakhomov <shtormless@gmail.com>
2021-02-04 21:40:48 -0500

    add few comments

--- lab1/main.go
+++ lab1/main.go
@@ -5,31 +5,34 @@ import (
 	"strconv"
 )
 
+// convert our array of integers to string
 func codeToText(code []int64) string {
 	var result string
 	for i := 0; i < len(code); i++ {
-		result = result + string(rune(code[i]))
+		result = result + string(rune(code[i])) // rune(65) == 'A' and so on
 	}
 	return result
 }
 
+// convert our text into array of integers
 func textToCode(text string) []int64 {
-	runes := []rune(text)
+	runes := []rune(text) // represent our text as runes
 
 	var result []int64
 
 	for i := 0; i < len(runes); i++ {
-		result = append(result, int64(runes[i]))
+		result = append(result, int64(runes[i])) // int64('A') == 65
 	}
 
 	return result
 }
 
+// convert our array of integers into binary
 func intToBin(code []int64) string {
 	var result string
 
 	for i := 0; i < len(code); i++ {
-		result = result + string(strconv.FormatInt(code[i], 2))
+		result = result + string(strconv.FormatInt(code[i], 2)) // => 77 == '1001101'
 	}
 
 	return result
