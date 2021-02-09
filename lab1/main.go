61a44119dafff8b5052ac867489aa77aedbe17d8
Bohdan Pakhomov <shtormless@gmail.com>
2021-02-08 20:46:13 -0500

    split functionality, scan container, chage container text

--- lab1/main.go
+++ lab1/main.go
@@ -2,56 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-// convert our array of integers to string
-func codeToText(code []int64) string {
-	var result string
-	for i := 0; i < len(code); i++ {
-		result = result + string(rune(code[i])) // rune(65) == 'A' and so on
-	}
-	return result
-}
-
-// convert our text into array of integers
-func textToCode(text string) []int64 {
-	runes := []rune(text) // represent our text as runes
-
-	var result []int64
-
-	for i := 0; i < len(runes); i++ {
-		result = append(result, int64(runes[i])) // int64('A') == 65
-	}
-
-	return result
-}
-
-// convert our array of integers into binary
-func intToBin(code []int64) string {
-	var result string
-
-	for i := 0; i < len(code); i++ {
-		result = result + string(strconv.FormatInt(code[i], 2)) // => 77 == '1001101'
-	}
-
-	return result
-}
-
-//func decode(container string) error {
-//	// TODO:
-//}
-//
 func main() {
-	message := "My hidden message..."
-	fmt.Println(message)
+	var message string
+	fmt.Println("Enter message to hide:")
+	fmt.Scanln(&message)
 
+	lines, err := scanLines("./container.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	// text to ASCII binCodes
 	encodedText := textToCode(message)
 	fmt.Println(encodedText)
-
+	// ASCII codes to binary
 	binCodes := intToBin(encodedText)
 	fmt.Println(binCodes)
-
-	decodedText := codeToText(encodedText)
+	// binary to ASCII codes
+	decCodes := binToInt(binCodes)
+	fmt.Println(decCodes)
+	// ASCII codes to text
+	decodedText := codeToText(decCodes)
 	fmt.Println(decodedText)
 }
---
+++ lab1/scanContainer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func scanLines(path string) ([]string, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
+}
