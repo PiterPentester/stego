bc8e793f0007f9808f82d1c64d0aa2dcbf61db72
Bohdan Pakhomov <shtormless@gmail.com>
2021-02-13 21:02:34 -0500

    looking for hidden text, refactored main

--- lab1/main.go
+++ lab1/main.go
@@ -7,37 +7,48 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Please enter text to hide:")
-	message, _ := reader.ReadString('\n')
+	mode := bufio.NewReader(os.Stdin)
+	fmt.Println("Please chose mode. h = hide message, s = seek message:")
+	modeAnswer, _ := mode.ReadString('\n')
+	if modeAnswer == "h\n" {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Please enter text to hide:")
+		message, _ := reader.ReadString('\n')
 
-	// text to ASCII binCodes
-	encodedText := textToCode(message)
-	fmt.Println(encodedText)
+		// text to ASCII binCodes
+		encodedText := textToCode(message)
+		//fmt.Println(encodedText)
 
-	// ASCII codes to binary
-	binCodes := intToBin(encodedText)
-	fmt.Println(binCodes)
+		// ASCII codes to binary
+		binCodes := intToBin(encodedText)
+		//fmt.Println(binCodes)
 
-	// scan container
-	lines, err := scanLines("./container.txt")
-	if err != nil {
-		panic(err)
+		// scan container
+		lines, err := scanLines("./container.txt")
+		if err != nil {
+			panic(err)
+		}
+
+		// hide message
+		res, err := hideMessage(binCodes, lines)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		writeToFile(res)
+
+	} else if modeAnswer == "s\n" {
+		// scan hidden text
+		hidden, err := scanLines("./hidden.txt")
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		// try to find hidden message
+		decodedText := seekMessage(hidden)
+		fmt.Println("Message found:")
+		fmt.Println(decodedText)
+	} else {
+		fmt.Println("Error: Choose right mode!")
 	}
-
-	// hide message
-	res, err := hideMessage(binCodes, lines)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println(res)
-
-	// binary to ASCII codes
-	decCodes := binToInt(binCodes)
-	fmt.Println(decCodes)
-
-	// ASCII codes to text
-	decodedText := codeToText(decCodes)
-	fmt.Println(decodedText)
 }
